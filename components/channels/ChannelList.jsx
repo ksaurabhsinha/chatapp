83d2f3c3193dc2754e52ae45effc9c2fb996ccf7
Kumar Saurabh Sinha <sinha.ksaurabh@gmail.com>
2017-08-05 09:05:06 +0200

    setup and channel and channellist component

---
+++ components/channels/ChannelList.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import Channel from './Channel';
+
+class ChannelList extends Component {
+
+    render() {
+        return (
+            <ul>{
+                this.props.channels.map(chan => {
+                    <Channel channel={chan} setChannel={this.props.setChannel}/>
+                })
+            }</ul>
+        )
+    }
+}
+
+ChannelList.propTypes = {
+    channels: React.PropTypes.array.isRequired,
+    setChannel: React.PropTypes.func.isRequired
+};
+
+export default ChannelList;
